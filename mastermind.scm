22719f1e0c99023b769f7a6ccd69e9be31942107
mukko <kyo22943@gol.com>
2014-11-11 20:10:10 +0900

    mastermindのランダムな数字リストを作成するmake-answerを完成

--- mastermind.scm
+++ mastermind.scm
@@ -7,4 +7,14 @@
 ;実装の際はjavaでランダムとかかな
 (use srfi-27)
 ;今回の上限は9なので10を設定
-(print (random-integer 10))
+
+;answer is empty list
+(define (make-answer answer)
+    (if (= (length answer) 4)
+      	answer
+           (let ((num (random-integer 10)))
+             (if (member num answer)
+                 (make-answer answer)
+                 (make-answer (cons num answer))))))
+
+(display (make-answer '()))
