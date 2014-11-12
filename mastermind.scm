f8f0076b2c32e6ccffd854145b5fff92de32312a
mukko <kyo22943@gol.com>
2014-11-12 11:13:50 +0900

    [WIP]入力処理をつくっているが、input-4-numbersのはじめのdisplayがでない...

--- mastermind.scm
+++ mastermind.scm
@@ -18,3 +18,31 @@
                  (make-answer (cons num answer))))))
 
 (display (make-answer '()))
+
+;input method
+(define (input-number)
+  (let ((num (read)))
+    (cond ((not (integer? num))
+           (display "input integer [0 - 9]\n")
+           #f)
+          ((<= 0 num 9) num)
+          (else
+            (display "input [0 - 9]\n")
+            #f))))
+
+;input 4 method
+(define (input-4-numbers)
+  (display "input four numbers\n>")
+  (let loop ((num-list '()))
+    (if (= (length num-list) 4)
+    	(reverse num-list)
+     	(let ((num (input-number)))
+        	(cond ((not num)
+                (delete-input-data)
+                (input-4-numbers))
+               	((member num num-list)
+                 (display "same number error\n")
+                 (delete-input-data)
+                 (input-4-numbers))
+                (else
+                  (loop (cons num numlist))))))))
