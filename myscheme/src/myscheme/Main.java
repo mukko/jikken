1deef459554f007442868c1d9e732b65aeb75956
mukko <kyo22943@gol.com>
2014-11-25 22:40:03 +0900

    Mainクラスを作成した
    とりあえずlexをつくるかな

--- myscheme/src/myscheme/Main.java
+++ myscheme/src/myscheme/Main.java
@@ -1,8 +1,23 @@
 package myscheme;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String args[])
+	public static void main(String args[])throws IOException
 	{
-		System.out.println("Main");
+		try{
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			String string = in.readLine();
+			System.out.println(string);
+	}catch(Exception e){
+		
+	}
+	}
+	
+	public void lexer()
+	{
+		
 	}
 }
