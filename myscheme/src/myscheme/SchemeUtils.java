5dcd55a7b12247b6fae11387c05463e780bbdc18
mukko <kyo22943@gol.com>
2014-11-30 18:01:11 +0900

    length,char,errorを追加

--- myscheme/src/myscheme/SchemeUtils.java
+++ myscheme/src/myscheme/SchemeUtils.java
@@ -1,5 +1,7 @@
 package myscheme;
 
+import java.io.PrintWriter;
+
 public abstract class SchemeUtils {
 	public static final Boolean TRUE = Boolean.TRUE;
 	public static final Boolean FALSE = Boolean.FALSE;
@@ -10,5 +12,28 @@ public abstract class SchemeUtils {
 	
 	public static Object rest(Object obj){
 		return (obj instanceof Pair) ? ((Pair) obj).rest : null;
+	}
+	
+	public static int length(Object obj){
+		int length = 0;
+		while(obj instanceof Pair){
+			length++;
+			obj = ((Pair) obj).rest;
+		}
+		return length;
+	}
+	
+	public static char chr(Object obj){
+		if(obj instanceof Character) return ((Character) obj).charValue();
+		else return chr(error("expected a char, got: " + obj));
+	}
+	
+	public static Character chr(char ch){
+		return new Character(ch);
+	}
+	
+	public static Object error(String message){
+		System.err.println("**** ERROR: " + message);
+		throw new RuntimeException(message);
 	}
 }
